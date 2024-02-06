c29ebba2021794f4de2fa0b7d0c560e06b2d274a
aji <hua_ji1998@163.com>
2024-02-06 17:57:14 +0800

    add(*):首页

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,1 +1,23 @@
-!
+<template>
+  <div class="indexwrap">
+    <div class="cardwrap">
+        <div>
+        <img src="" alt="">
+        <p>我是玩家</p>
+        </div>
+        <div>
+        <img src="" alt="">
+        <p>我是城主</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scope lang='sass'>
+.cardwrap{
+    
+}
+</style>
